413d77526c3af6241696b382c89f5c72ac72096f
Filip <filip@MacBook-Air-Filip.local>
2025-05-29 20:06:01 +0200

    przeniesienie katalogu nauka

---
+++ nauka/golang/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Przykłady deklaracji zmiennych
+
+	wiek := 25
+	imie := "Jan"
+	nazwisko := "Kowalski"
+	wzrost := 1.80
+	aktywny := true
+
+	// Wyświetlenie zmiennych
+	fmt.Println("Wiek: ", wiek)
+	fmt.Println("Imię i nazwisko: ", imie, nazwisko)
+	fmt.Println("Wzrost: ", wzrost)
+	fmt.Println("Aktywny: ", aktywny)
+}
